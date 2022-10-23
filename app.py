5aac7ab295f7013ebc14b8aecf510b6550109901
Rakesh <rakesh.sukla53@gmail.com>
2022-10-23 13:41:35 -0400

    adding gitignore

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+import pathlib
+import requests
+
+
+with open("base.json", "r") as f:
+    config = json.load(f)
+
+
+def get_users() -> dict:
+    r = requests.get(config["api"]["url"])
+    return r.text
+
+
+if __name__ == "__main__":
+    print(get_users())
+
+
